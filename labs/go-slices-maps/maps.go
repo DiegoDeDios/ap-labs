95cadf1c791badef3592c321ad82524b6027dec2
Diego <diegomzdedios@gmail.com>
2019-09-11 01:59:25 +0000

    Maps y Slices resueltos

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
-func main() {
+import (
+	"strings"
+	"fmt"
+)
+
+func WordCount(s string) map[string]int {
+	m:= make(map[string]int);
+	for _, word := range strings.Fields(s){
+		m[word] += 1;
+		}
+	return m;
 }
+
+func main() {
+	fmt.Printf("El mapa es: %v\n",WordCount("Hola mundo esto es un lab"));
+}
+
