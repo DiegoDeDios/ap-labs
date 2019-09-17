c20f2f0caca20c95cc63149c4b0b91de2eae73fa
Diego <diegomzdedios@gmail.com>
2019-09-17 01:19:39 +0000

    Lab 1.4 completed

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    return 0;
+int mystrlen(char*);
+char* mystradd(char*, char*);
+int mystrfind(char*, char*);
+
+int main(argc, argv)
+char argc;
+char** argv;
+{
+   if(argc < 4){
+	printf("You need to pass 3 arguments\n");
+	return -1;
+	}
+   int strLen = mystrlen(argv[1]);
+   char* strAdd = mystradd(argv[1],argv[2]);
+   char* isSubstr = mystrfind(strAdd,argv[3]) ? "yes":"no";
+   printf("Initial Length\t: %d\nNew String\t: %s\nSubString was found\t: %s\n", strLen, strAdd, isSubstr); 
+   return 0;
 }
