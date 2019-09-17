6cc91a1f07d74e372d226cd6a7beb9864f2960f0
Diego <diegomzdedios@gmail.com>
2019-09-17 01:19:39 +0000

    Lab 1.4 completed

--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,41 @@
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int mystrlen(char *str){
-    return 0;
+        const char *s;
+        for(s = str; *s; ++s);
+        return (s-str);
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int originalLength = mystrlen(origin);
+    int additionLength = mystrlen(addition);
+    char* newString = malloc(sizeof(char)*originalLength + additionLength);
+    strcpy(newString, origin);
+    strcpy(newString + originalLength, addition);
+    return newString;
 }
 
 int mystrfind(char *origin, char *substr){
+    char* a = origin;
+    char* b = substr;
+    while(a < origin + mystrlen(origin)){
+      if(*a == *b){
+        if(b == substr + mystrlen(substr) - 1){
+          return 1;
+        }
+          a+=1;
+          b+=1;
+      }
+      else{
+          a+=1;
+          b = substr;
+      } 
+    }
     return 0;
 }
+
+
+
