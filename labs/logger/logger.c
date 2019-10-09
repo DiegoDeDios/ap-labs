138a1181155d466382aeeb65d549a73361df7d8e
Diego <diegomzdedios@gmail.com>
2019-10-09 00:15:16 +0000

    Added support for formats

--- labs/logger/logger.c
+++ labs/logger/logger.c
@@ -1,53 +1,34 @@
 #include <stdarg.h>
 #include "colors.h"
-int infof(const char *format, ...){
+void infof(const char *format, ...){
     va_list arg;
-
     va_start(arg, format);
     textcolor(BRIGHT, BLUE, BLACK);
-    while (format) {
-        printf("%s\n",format);
-        format = va_arg(arg, const char *);
-    }
+    vfprintf (stdout, format, arg);
     textcolor(RESET, WHITE, BLACK);	
     va_end(arg);
 }
-int warnf(const char *format, ...){
+void warnf(const char *format, ...){
     va_list arg;
-
     va_start(arg, format);
     textcolor(BRIGHT, YELLOW, BLACK);
-
-    while (format) {
-        printf("%s\n",format);
-        format = va_arg(arg, const char *);
-    }
+    vfprintf (stdout, format, arg);
     textcolor(RESET, WHITE, BLACK);	
     va_end(arg);
 }
-int errorf(const char *format, ...){
+void errorf(const char *format, ...){
     va_list arg;
-
     va_start(arg, format);
     textcolor(BRIGHT, RED, BLACK);
-
-    while (format) {
-        printf("%s\n",format);
-        format = va_arg(arg, const char *);
-    }
+    vfprintf (stdout, format, arg);
     textcolor(RESET, WHITE, BLACK);	
     va_end(arg);
 }
-int panicf(const char *format, ...){
+void panicf(const char *format, ...){
     va_list arg;
-
     va_start(arg, format);
     textcolor(BRIGHT, RED, BLACK);
-
-    while (format) {	
-        printf("%s\n",format);
-        format = va_arg(arg, const char *);
-    }
+	vfprintf (stdout, format, arg);
     textcolor(RESET, WHITE, BLACK);	
     va_end(arg);
 }
--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 void main(){
-infof("Info",NULL);
-warnf("Advertencia",NULL);
-errorf("Error",NULL);
-panicf("Panico",NULL);
+int x = 10;
+char* y = "warning";
+float z = 10.0f;
+infof("Info, valor: %d \n",x);
+warnf("Advertencia, valor: %s\n",y);
+errorf("Error, valor: %f \n",z);
+panicf("Panico\n");
 }
