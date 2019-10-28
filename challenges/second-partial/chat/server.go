451fb0393339bc804df1011c85c6dc5334208794
Diego <diegomzdedios@gmail.com>
2019-10-28 03:53:21 +0000

    Second Exam completed

--- challenges/second-partial/chat/server.go
+++ challenges/second-partial/chat/server.go
@@ -9,19 +9,101 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"strings"
+	"time"
+	"os"
+	"math/rand"
+	"reflect"
 )
 
 //!+broadcaster
 type client chan<- string // an outgoing message channel
-
+var users map[string]net.Conn //Map of users
+var moderator string
+var isFirstConn bool
 var (
 	entering = make(chan client)
 	leaving  = make(chan client)
 	messages = make(chan string) // all incoming client messages
 )
+
+
+func getUsers() string {
+	//Display info of connected users
+	var buf bytes.Buffer
+	buf.WriteString("server > Users on this channel:\n")
+	buf.WriteString("--------------\n")
+	for k, _ := range users {
+		buf.WriteString(k + " ")
+	}
+	buf.WriteString("\n--------------\n")
+	return buf.String()
+}
+
+func getUserInfo(user string) string {
+	if _, ok := users[user]; ok { 
+		return ("server > " + user + "IP: " + users[user].RemoteAddr().String())
+	} else {
+		return "Unavailable user"
+	}
+
+}
+
+func getTime() string {
+	var buf bytes.Buffer
+	buf.WriteString("server > Current time: ")
+	buf.WriteString(time.Now().Format(time.RFC850))
+	return buf.String()
+}
+
+func getMod() string {
+	var buf bytes.Buffer
+	buf.WriteString("server > The moderator is ")
+	buf.WriteString(moderator)
+	return buf.String()
+}
+
+func sendPrivateMessage(origin, destination string, data []string) {
+	if _, ok := users[destination]; ok { 
+		fmt.Fprintln(users[destination], origin+"> "+strings.Join(data, " "))
+	} else {
+		fmt.Fprintln(users[destination], "Invalid username")
+	}
+
+}
+
+func kickUser(kicker, banned string) string {
+	if _, ok := users[banned]; ok { 
+		if strings.Compare(kicker, moderator)!=0{
+			return "Only the moderator can kick users from the channel"
+		} else{
+			users[banned].Close()
+			delete(users, banned)
+			return "server >" + banned + "has been kicked out"
+		}
+	} else {
+		return "User doesn't exist"
+	}
+}
+
+
+func setModerator(user string){
+	moderator = user
+}
+
+func isModerator(user string) bool {
+	return strings.Compare(user,moderator) == 0
+}
+
+func SelectNewModerator(mapI interface{}) interface{} {
+	keys := reflect.ValueOf(mapI).MapKeys()
+	return keys[rand.Intn(len(keys))].Interface()
+}
 
 func broadcaster() {
 	clients := make(map[client]bool) // all connected clients
@@ -47,27 +129,78 @@ func broadcaster() {
 //!-broadcaster
 
 //!+handleConn
-func handleConn(conn net.Conn) {
-	ch := make(chan string) // outgoing client messages
-	go clientWriter(conn, ch)
-
-	who := conn.RemoteAddr().String()
-	ch <- "You are " + who
-	messages <- who + " has arrived"
-	entering <- ch
-
-	input := bufio.NewScanner(conn)
-	for input.Scan() {
-		messages <- who + ": " + input.Text()
+func handleConn(conn net.Conn, user string) {
+users[user] = conn
+ch := make(chan string) // outgoing client messages
+if isFirstConn {
+	setModerator(user)
+	isFirstConn = false
+}
+go clientWriter(conn, ch, user)
+ch <- "server > User " + user + " has joined the channel!"
+if strings.Compare(user, moderator) == 0 {
+	ch <- "server >" + user + " you are the moderator!"
+}
+entering <- ch
+input := bufio.NewScanner(conn)
+for input.Scan() {
+	data := input.Text()
+	data_len := len(data)
+	if data_len <= 0 {
+		messages <- user + "> " + input.Text()
+		continue
 	}
-	// NOTE: ignoring potential errors from input.Err()
+	if strings.Compare(string(data[0]), "/") != 0 {
+		messages <- user + "> " + input.Text()
+		continue
+	}
+	command := strings.Split(
+		strings.Trim(data, " "),
+		" ")
+	if len(command) == 1 {
+		switch command[0] {
+		case "/users":
+			ch <- getUsers()
+		case "/time":
+			ch <- getTime()
+		case "/moderator":
+			ch <- getMod()
+		default:
+			messages <- user + "> " + data
+		}
+	} else if len(command) == 2 {
+		if strings.Compare(command[0], "/user") == 0 {
+			ch <- getUserInfo(command[1])
+		} 
+		if strings.Compare(command[0], "/kick") == 0 {
+			ch <- kickUser(user, command[1])
+		} else {
+			messages <- user + "> " + data
+		}
+	} else if len(command) >= 3 {
+		if strings.Compare(command[0], "/msg") == 0 {
+			sendPrivateMessage(user, command[1], command[2:])
+		}
 
-	leaving <- ch
-	messages <- who + " has left"
-	conn.Close()
+	}
+
 }
 
-func clientWriter(conn net.Conn, ch <-chan string) {
+if isModerator(user){
+	moderator = SelectNewModerator(users).(string)
+}
+messages <- "server > " + moderator + " is now the moderator!"
+delete(users, user)
+
+fmt.Println("server >", user, "left")
+
+leaving <- ch
+messages <- user + " has left"
+
+conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string, user string) {
 	for msg := range ch {
 		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
 	}
@@ -77,11 +210,26 @@ func clientWriter(conn net.Conn, ch <-chan string) {
 
 //!+main
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	var host string
+	var port string
+	flag.StringVar(&host, "host", "localhost", "server address")
+	flag.StringVar(&port, "port", "8000", "port to listen")
+	flag.Parse()
+	isFirstConn = true
+	listener, err := net.Listen("tcp", host+":"+port)
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(-1);
 	}
-
+	users = make(map[string]net.Conn)
+	fmt.Println("Server initializing...")
+	fmt.Println(time.Now().UTC())
+	fmt.Println("server> Go server running on:", host, port)
+	fmt.Println("Remember the guidelines of this channel:")
+	fmt.Println("- No swearing allowed, be nice to each other")
+	fmt.Println("- No harrasment or bullying")
+	fmt.Println("- No sexual undertones allowed")
+	fmt.Println("- Failure to comply will result in inmediate banning by the moderator")
 	go broadcaster()
 	for {
 		conn, err := listener.Accept()
@@ -89,7 +237,15 @@ func main() {
 			log.Print(err)
 			continue
 		}
-		go handleConn(conn)
+		user, _ := bufio.NewReader(conn).ReadString('\n')
+		user = user[:len(user)-1]
+		//Test is user exists
+		if _, exists := users[user]; exists {
+			fmt.Fprintf(conn, "server > Not a valid username, try another one\n")
+			conn.Close()
+		} else {
+			go handleConn(conn, user)
+		}
 	}
 }
 
--- challenges/second-partial/mytop/mytop.c
+++ challenges/second-partial/mytop/mytop.c
@@ -1,13 +1,218 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
+#include <dirent.h> 
+#include <ctype.h>
+#include <errno.h>
+#include <sys/inotify.h>
+#define BUF_LEN sizeof(struct inotify_event) * 1024
+//Global variables -----
+int IDS[1024];
+int fd;
+int wd;
+int rd;
+char* p;
+struct inotify_event *event;
+//--------------------------
+
+
+struct Process{
+  int pid; //Field 1
+  int parent; //Field 4
+  char* name; //Field 2
+  char* state; //Field 3
+  int memory;  // Field 23
+  int threads; // Field 20 of stat
+  int op_files; // located at subdirectory id/fd
+};
+
 
 void clear();
+int getProcessList();
+char isProcess(char* id);
+int countFD(int pid);
+void listProcess(int pid);
+int openFile(char* filename);
+off_t getSize(int fd);
+void eventHandler(struct inotify_event* event);
+void top();
 
 int main(){
-  // Place your magic here
-  clear();
+
+  //Place a watcher on the proc directory
+    fd = inotify_init1(O_NONBLOCK);
+    wd = inotify_add_watch(fd, "/proc", IN_ALL_EVENTS);
+    char* buffer = (char*)malloc(BUF_LEN);
+
+
+  //-------
+
+  top();
+
+
+  //Event handler
+      while(1){
+        rd = read(fd, buffer, BUF_LEN);
+        p = buffer;
+        event = (struct inotify_event*)p;
+        for (p = buffer; p < buffer + rd; ) {
+             event = (struct inotify_event *) p;
+             eventHandler(event);
+             p += sizeof(struct inotify_event) + event->len;
+         }
+    }
+    close(fd);
+
   return 0;
+}
+
+void top(){
+  int proc = getProcessList();
+  printf("|%5s|%6s|%19s|%10s|%8s|%5s|%5s|\n", "PID","Parent","Name","State","Memory","Threads","Files");
+  for(int i = 0; i < proc; i++){
+     listProcess(IDS[i]);
+  }
+}
+void eventHandler(struct inotify_event* event){
+     if (event->mask & IN_CREATE){      
+          clear();
+          top();
+      }
+     if (event->mask & IN_DELETE){      
+        clear();
+        top();
+       }
+     if (event->mask & IN_MODIFY){     
+        clear();
+        top();
+        }
+}
+
+int openFile(char* filename){
+    int fd = open(filename, O_RDONLY);
+    return fd;
+}
+
+off_t getSize(int fd){
+    off_t curOffset = lseek(fd, (size_t)0, SEEK_CUR);
+    off_t size = lseek(fd, (size_t)0, SEEK_END); 
+    lseek(fd, curOffset, SEEK_SET);
+    return size;
 }
 
 void clear() {
   printf("\e[1;1H\e[2J"); 
+}
+
+void listProcess(int pid){
+    char filename[512];
+    sprintf(filename, "/proc/%d/stat", pid);
+    FILE *f = fopen(filename, "r");
+    int unused;
+    char* comm = (char*)malloc(sizeof(char)*256);
+    char state;
+    int ppid;
+    int threads;
+    unsigned int mem;
+    fscanf(f, "%*d %s %c %d %*d %*d %*d %*d %*u %*lu %*lu %*lu %*lu %*lu %*lu %*ld %*ld %*ld %*ld %d %*ld %*llu %u", comm, &state, &ppid, &threads, &mem);
+    fclose(f);
+
+    int op_files = countFD(pid);
+    struct Process* newProc = (struct Process*)malloc(sizeof(struct Process));
+    newProc->pid = pid;
+    newProc->name = comm;
+    newProc->parent = ppid;
+    newProc->state = state;
+
+    switch (state)
+    {
+    case 'R':
+    newProc->state = "Running";
+      break;
+    case 'S':
+    newProc->state = "Sleeping";
+      break;
+    case 'D':
+    newProc->state = "Idle";
+      break;
+    case 'Z':
+    newProc->state = "Zombie";
+      break;
+    case 'T':
+    newProc->state = "Stopped";
+      break;
+    case 't':
+    newProc->state = "Trace stopped";
+      break;
+    case 'X':
+    newProc->state = "Dead";
+      break;
+    case 'x':
+    newProc->state = "Dead";
+      break;
+    case 'K':
+    newProc->state = "Wakekill";
+      break;
+    case 'W':
+    newProc->state = "Waking";
+      break;
+    case 'P':
+    newProc->state = "Parked";
+      break;
+    default:
+    newProc->state = "Idle";
+      break;
+    } 
+    newProc->op_files = op_files;
+    newProc->threads = threads;
+    newProc->memory = mem/10000000;
+    printf("|%5d|%6d|%19s|%10s|%8luM|%5d|%5d| \n", newProc->pid, newProc->parent, newProc->name, newProc->state, newProc->memory, newProc->threads, newProc->op_files);
+    free(newProc);
+} 
+
+int countFD(int pid){
+  char fdir[512];
+  sprintf(fdir, "/proc/%d/fd", pid);
+  int fd_open = 0;
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(fdir);
+  if (d) {
+    while ((dir = readdir(d)) != NULL) {
+      fd_open++;
+    }
+    closedir(d);
+  }
+  return fd_open;
+}
+
+char isProcess(char* id){
+    size_t length = strlen(id);
+    for (int i=0;i<length; i++){
+      if (!isdigit(id[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int getProcessList(){ //Returns number of processes
+  int i = 0;
+  DIR *d;
+  struct dirent *dir;
+  d = opendir("/proc");
+  if (d) {
+    while ((dir = readdir(d)) != NULL) {
+      if(isProcess(dir->d_name)){
+        IDS[i++] = atoi(dir->d_name);
+        //printf("%s\n", dir->d_name);
+      }
+    }
+    closedir(d);
+  }
+  return i;
 }
