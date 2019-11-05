b377ea4dae4b08d4d1ccd800ba9b4877874d1ffe
Diego <diegomzdedios@gmail.com>
2019-11-05 16:09:28 +0000

    Added backup on new branch

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+void swap(char* a, char*b){ // I wanted to implement an in-place swap
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
 }
+
+void reverse(char* buffer, int size ){
+       char* a = buffer;
+       char* b = buffer + size - 1;
+       for(char i = 0; i < size/2; i++){
+           swap(a++, b--);
+       }
+}
+
+ int main(){
+    char c = 0;
+    char buffer[64];
+    while((c = getchar()) != EOF ){
+       scanf("%s", buffer);
+       reverse(buffer,strlen(buffer));
+       printf("%s\n", buffer);
+       }
+     return 0;
+ }
+
